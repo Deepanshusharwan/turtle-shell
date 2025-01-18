5ae28e03ac844df6882d18a2cedca8cd022f2773
Deepanshusharwan <deepanshusharwan35@gmail.com>
2025-01-18 20:26:19 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
 
     # Wait for user input
     user_input = input()
-    print(f"{user_input}: command not found"
+    print(f"{user_input}: command not found")
 
 
 if __name__ == "__main__":
