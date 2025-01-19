05d4b0fb87bd2633a23f15f771e53066fdc5c572
Deepanshusharwan <deepanshusharwan35@gmail.com>
2025-01-19 06:44:17 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+from os.path import split
 
 
 def main():
@@ -6,9 +7,34 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         user_input = input()
-        if user_input == "exit 0":
-            sys.exit(0)
-        print(f"{user_input}: command not found")
+        user_command = user_input.split()
+
+
+        if user_command[0] == 'exit' and len(user_command) == 2:  # for exit commands
+            try:
+                sys.exit(int(user_command[1]))
+            except ValueError:
+                print("the exit code must be an integer")
+
+        elif user_command[0] == "exit" and len(user_command) != 2:
+            print(f"{user_command[0]} requires one argument, exit code.")
+
+
+        #for echo command
+        if user_command[0] == "echo":
+            print(" ".join(user_command[1:]))
+
+
+        else:
+            print(f"{user_input}: command not found")
+
+'''
+def exit(user_command: list):
+    if user_command[0] == 'exit' and len(user_command) == 2:  # for exit commands
+        sys.exit(int(user_command[1]))
+
+    elif user_command[0] == "exit" and len(user_command) != 2:
+        print(f"{user_command[0]} requires one argument, exit code.")'''
 
 
 
