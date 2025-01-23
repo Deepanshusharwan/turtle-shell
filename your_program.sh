b4c66037cc42235457f7e5a29bd7d94c96209980
Deepanshusharwan <deepanshusharwan35@gmail.com>
2025-01-23 06:50:22 +0530

    initial commit

--- your_program.sh
+++ your_program.sh
@@ -8,8 +8,8 @@
 
 set -e # Exit early if any commands fail
 
-# Copied from .codecrafters/run.sh
+# Copied from .bash_scripts/run.sh
 #
 # - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
+# - Edit .bash_scripts/run.sh to change how your program runs remotely
 exec pipenv run python3 -u -m app.main "$@"
